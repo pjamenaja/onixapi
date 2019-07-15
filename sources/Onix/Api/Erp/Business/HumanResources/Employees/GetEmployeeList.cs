a5d9ff99c776155d88790ca27496370a7a4a468e
pjamenaja <pjame.fb@gmail.com>
2019-07-16 06:46:07 +0700

    Test Sonarqube rule

--- sources/Onix/Api/Erp/Business/HumanResources/Employees/GetEmployeeList.cs
+++ sources/Onix/Api/Erp/Business/HumanResources/Employees/GetEmployeeList.cs
@@ -18,8 +18,16 @@ namespace Onix.Api.Erp.Business.HumanResources.Employees
             CTable o = new CTable();
             BusinessOperationUtils.PopulateRow(qe, o, "EMPLOYEE_LIST", rows);
 
-            int j = 0;
-            int i = 10/j;
+            String s = "Hello";
+            String k = "Hello";
+            if (k == s)
+            {
+
+            }
+            else if (k != s)
+            {
+                
+            }
 
             return o;
         }
