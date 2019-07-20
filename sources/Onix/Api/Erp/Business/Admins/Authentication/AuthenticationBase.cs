5f019836b75a83407de964ba5f70e1d2ed4349a8
pjamenaja <pjame.fb@gmail.com>
2019-07-20 12:17:11 +0700

    Added test coverage

--- sources/Onix/Api/Erp/Business/Admins/Authentication/AuthenticationBase.cs
+++ sources/Onix/Api/Erp/Business/Admins/Authentication/AuthenticationBase.cs
@@ -1,6 +1,5 @@
 using System;
 
-using Onix.Api.Commons;
 using Onix.Api.Commons.Business;
 using Onix.Api.Erp.Business.Admins.AuthenProviders;
 
@@ -8,8 +7,13 @@ namespace Onix.Api.Erp.Business.Admins.Authentication
 {    
 	public class AuthenticationBase : BusinessOperationBase
 	{
-        private IAuthenProvider authenProvider = new AuthenProviderFirebase();
+        private IAuthenProvider authenProvider = null;
         
+        public AuthenticationBase()
+        {
+            authenProvider = new AuthenProviderFirebase();
+        }
+
         protected IAuthenProvider getAuthenticationProvider()
         {
             return authenProvider;
